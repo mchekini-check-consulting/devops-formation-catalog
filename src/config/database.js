ad4a6f29272ee2e5c8ecf9f0c78fe6bf80d45052
araoufch <charefraouf919@gmail.com>
2026-04-20 21:22:22 +0200

    chore: postgres port

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'postgres',
   {
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 5432,
+    port: parseInt(process.env.DB_PORT) || 5434,
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
